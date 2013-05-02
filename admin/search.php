34d40dd4710550721a7e86507710854abedb50cb
geraldbullard <gerald.bullard@loadedcommerce.com>
2013-05-01 22:39:53 -0400

    more search updates

--- admin/search.php
+++ admin/search.php
@@ -9,7 +9,7 @@
     if (mysql_num_rows($query)) {
       $string .= "<ul>";
       while ($row = mysql_fetch_assoc($query)) {
-        $string .= '<li onMouseOver="change_background_color(\'eeeeee\');"><a href="index.php?action=listContent&categoryId=' . (($row['type'] == 0) ? $row['id'] : $row['categoryId']) . '">' . $row['title'] . '</a></li>' . "\n";
+        $string .= '<li>' . (($row['type'] == 0) ? '<i class="icon icon-orange icon-folder-open"></i>' : '<i class="icon icon-orange icon-document"></i>') . ' <a href="index.php?action=listContent&categoryId=' . (($row['type'] == 0) ? $row['id'] : $row['categoryId']) . '">' . $row['title'] . '</a></li>' . "\n";
       }
       $string .= "</ul>"; 
     } else {
