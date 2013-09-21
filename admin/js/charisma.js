1125f988bbcdee65d0238246e1b7e17a954c80d2
geraldbullard <gerald.bullard@gmail.com>
2013-09-20 20:18:06 -0400

    removed unwanted merge codes

--- admin/js/charisma.js
+++ admin/js/charisma.js
@@ -592,8 +592,7 @@ function toggleLeftNav() {
     $("#rightContent .box-header h2 .icon-chevron-right").hide();
     $("#leftNav").show();
     $("#rightContent").removeClass("span12").addClass("span9").removeAttr("style");
-  }        
-<<<<<<< HEAD
+  }
 }
 
 function checkChevronUpDown(el) {
@@ -628,6 +627,4 @@ function sideToolsToggle() {
 function sideSettingsToggle() {
   checkChevronUpDown($("#settingsChevron"));
   $('#side_settings_menu').toggle(300);
-=======
->>>>>>> 36d67b06a7322f5dd0859e816718a33cca9bd57c
 }
